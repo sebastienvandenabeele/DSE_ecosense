c64b570b11fd194bed189055c71ab23c1b10a0fa
luptmoor <luptmoor@student.tudelft.nl>
2022-05-18 17:12:27 +0200

    Materials docstrings

--- Blimp/Classes/materials.py
+++ Blimp/Classes/materials.py
@@ -1,9 +1,17 @@
 class Material:
-    def __init__(self, vol_density, area_density, E, tensile_strength):
-        self.vol_density = vol_density            # [kg/m^3]
-        self.area_density = area_density          # [kg/m^2]
-        self.E = E                                # [Pa]
-        self.tensile_strength = tensile_strength  # [Pa]
+
+    def __init__(self, vol_density=0, area_density=0, E=0, tensile_strength=0):
+        """
+        A class defining materials
+        :param vol_density: [float] Volumetric density of material [kg/m^3]
+        :param area_density: [float] Area density of material [kg/m^2]
+        :param E: [float] Young's modulus of material [GPa]
+        :param tensile_strength: [float] Tensile strength of material [MPa]
+        """
+        self.vol_density = vol_density
+        self.area_density = area_density
+        self.E = E / 10**9                               # conversion to Pa
+        self.tensile_strength = tensile_strength / 10**6   # conversion to Pa
 
 
 
