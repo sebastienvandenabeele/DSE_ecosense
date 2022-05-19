347c501afc4af4c0265765a4fc9a4da538744187
Constantinos Aristodemou <50957278+ConstantinosAr@users.noreply.github.com>
2022-05-19 15:07:54 +0200

    Added a component in electronics

--- Blimp/Classes/electronics.py
+++ Blimp/Classes/electronics.py
@@ -1,22 +1,26 @@
 class Electronic:
-    def __init__(self, name, mass, power_consumption, cost):
+    def __init__(self, name, mass, power_consumption, cost, type):
         """
         A class describing any secondary electronic component
         :param: name: [str] Name of component
         :param mass: [float] Mass of component [kg]
         :param power_consumption: Steady-state power consumption [W]
         :param cost: Unit cost of component [EUR]
+        :param type: [str] Type of the component 
         """
 
         self.name = name
         self.mass = mass
         self.power_consumption = power_consumption
         self.cost = cost
+        self.type = type
 
 
 #####################
 # Electronics library
 #####################
+
+ZED_F9P = Electronic("ZED_F9P", 0.005, 0.204, 189.99, "GPS Module")
 
 
 ############################################
