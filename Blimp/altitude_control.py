f8e5c6ae959f2121965a1ad65759d413d871eba9
luptmoor <luptmoor@student.tudelft.nl>
2022-06-12 22:17:23 +0200

    cleaning of code

--- Blimp/altitude_control.py
+++ Blimp/altitude_control.py
@@ -152,16 +152,15 @@ def getC(blimp):
     return c
 
 
-def longitudinalStateSpace(blimp, d_eng, U):
+def longitudinalStateSpace(blimp, U):
     V = blimp.cruiseV
     VVh_sq = 0.75**2
-    # = 1
+
     dyn_pressure = 0.5 * getISA('rho', blimp.h_trim) * V**2
     S = blimp.ref_area
     C_m_q_hat = 0.073 # from Blibble, based on Solar HALE p 286
     C_L_q_hat = 0.024  # from Blibble, based on Solar HALE p 286
-    T1 = blimp.cruise_thrust / 2
-    T0 = blimp.cruise_thrust
+
     I_yy = blimp.Iyy
     k_atm = getK(blimp)
     c_atm = getC(blimp)
@@ -171,30 +170,30 @@ def longitudinalStateSpace(blimp, d_eng, U):
     x_ac = blimp.length * (0.5 - 0.37)   # Assumed at 37% length, from Blibble p 103
     z_cg = - blimp.z_cg
     x_fin = (blimp.x_l_fins - 0.5) * blimp.length
-
+    x_hinge = x_fin + 0.55 * blimp.fin.root_chord
 
     # Coefficients for model
-    C_w    = blimp.MTOW / (dyn_pressure * S)
-
-    C_T1   = T1 / (dyn_pressure * S)
     K_yy   = I_yy / (dyn_pressure * S * l_ref)
     C_k    = k_atm / (dyn_pressure * S)
-    C_mtom = blimp.MTOM / (dyn_pressure * S)
+    mu = blimp.MTOM / (dyn_pressure * S)
     C_c    = c_atm / (dyn_pressure * S)
+
     C_L_a_e = 2 / blimp.spheroid_ratio
     C_L_a_h = blimp.fin.CLa * 0.1995 * 0.5
+
     C_m_q_e =  C_m_q_hat * l_ref / V
     C_m_q_h = 0.1995 * C_L_a_h * (x_fin/l_ref)**2 # Blibble p 283
     C_m_q = C_m_q_h + C_m_q_e
 
+    C_F_delta = C_L_a_h * 0.9 * VVh_sq
     k_fin = 1/(blimp.fin.AR * 0.7 * np.pi)
 
 
-    C_m_a = C_L_a_e * x_ac / l_ref - C_L_a_h * x_fin / l_ref - C_w * z_cg / l_ref
+    C_m_a = C_L_a_e * x_ac / l_ref - C_L_a_h * x_fin / l_ref - mu * g * z_cg / l_ref
     print('Static stability, CMalpha = ', C_m_a)
 
     C1 = np.array([[1, 0, 1, 0, 0],
-                   [0, 0, -C_mtom*V, 0, 0],
+                   [0, 0, -mu*V, 0, 0],
                    [0, 1, 0, 0, 0],
                    [0, 0, 0, 0, -K_yy],
                    [0, 0, 0, 1, 0]])
@@ -202,20 +201,15 @@ def longitudinalStateSpace(blimp, d_eng, U):
     C2 = np.array([[0, 0, 0, 0, -1],
                    [C_L_a_e + C_L_a_h*VVh_sq, -C_k, -C_c * V, 0, 0],
                    [0, 0, -V, 0, 0],
-                   [C_L_a_e * x_ac/l_ref - C_L_a_h*VVh_sq * x_fin/l_ref, 0, 0, -C_w * z_cg/l_ref, -C_m_q],
+                   [C_L_a_e * x_ac/l_ref - C_L_a_h*VVh_sq * x_fin/l_ref, 0, 0, -mu * g * z_cg/l_ref, -C_m_q],
                    [0, 0, 0, 0, -1]])
 
-    C3 = np.array([[0],
-                  [2/(rho*V**2*S)],
-                  [0],
-                  [2/(rho*V**2*S) * d_eng / l_ref],
-                  [0]])
 
     # Elevator solution
     C3 = np.array([[0],
-                   [-blimp.fin.CLa * 0.1995*VVh_sq * 0.5],
+                   [-C_F_delta],
                    [0],
-                   [blimp.fin.CLa * 0.1995 * 0.5*VVh_sq * x_fin / l_ref],
+                   [C_F_delta * x_hinge / l_ref],
                    [0]])
 
     A = -np.linalg.inv(C1) @ C2  # State Matrix
@@ -227,22 +221,16 @@ def longitudinalStateSpace(blimp, d_eng, U):
     ml.damp(sys)
 
 
-
-    nu = 2 * np.arctan(U/T0)
-
-    throttle_up = 2 / (1 + np.cos(nu))
-
-    new_throttle = blimp.cruise_throttle * throttle_up
+    # nu = 2 * np.arctan(U/T0)
+    # throttle_up = 2 / (1 + np.cos(nu))
+    # new_throttle = blimp.cruise_throttle * throttle_up
 
     # print("Actual input of ", round(U, 2), 'N')
     # print('thrust vectored at ', round(nu * 57.3, 2), 'degrees')
     # print('throttle at ', round(new_throttle, 2), ', ', round((throttle_up-1)*100, 1), '% higher than cruise')
 
     ts = np.arange(0, 30, 0.1)
-
     us = np.ones(len(ts)) * U
-
-
     ys, ts_, xs = ml.lsim(sys, us, ts)
 
     plt.subplot(326)
@@ -318,64 +306,35 @@ def lateralStateSpace(blimp, u):
     C_N_r_hat = 0.073  # from Blibble, based on Solar HALE p 286
     C_Y_r_hat = 0.024   # ''
 
-    l_ref = blimp.length
-
     # Moment Arms
+    l_ref = blimp.length
     x_ac = blimp.length * (0.5 - 0.37)  # Assumed at 37% length, from Blibble p 103
     x_fin = (blimp.x_l_fins - 0.5) * blimp.length
     x_hinge = x_fin + 0.55 * blimp.fin.root_chord
 
     # Coefficients for model
     K_zz = 800 / (dyn_pressure * S * l_ref)# TODO
-    C_mtom = blimp.MTOM / (dyn_pressure * S)
-    C_L_beta_e = 2 / blimp.spheroid_ratio
-    C_L_beta_v = blimp.fin.CLa * 0.1995 * 0.5 * VVh_sq
+    mu = blimp.MTOM / (dyn_pressure * S)
+    C_Y_beta_e = 2 / blimp.spheroid_ratio
+    C_Y_beta_v = blimp.fin.CLa * 0.1995 * 0.5 * VVh_sq
 
     C_N_r_e = C_N_r_hat * np.sqrt(S) / V
-    C_N_r_t = -0.1995 * C_L_beta_v * (x_fin / l_ref) ** 2 * VVh_sq   # Blibble p 283
+    C_N_r_t = -0.1995 * C_Y_beta_v * (x_fin / l_ref) ** 2 * VVh_sq   # Blibble p 283
     C_N_r = C_N_r_t + C_N_r_e
 
-    C_L_r_e = C_Y_r_hat * np.sqrt(S) / V
-    C_L_r_t = 0.1995 * C_L_beta_v * x_fin / np.sqrt(S) * VVh_sq
-    C_L_r = C_L_r_t + C_L_r_e
+    C_Y_r_e = C_Y_r_hat * np.sqrt(S) / V
+    C_Y_r_t = 0.1995 * C_Y_beta_v * x_fin / np.sqrt(S) * VVh_sq
+    C_Y_r = C_Y_r_t + C_Y_r_e
 
-    C_F_delta = C_L_beta_v * 0.9 * VVh_sq
+    C_F_delta = C_Y_beta_v * 0.9 * VVh_sq
 
-    K_ac = (C_L_beta_e * x_ac/l_ref - C_L_beta_v * x_fin/l_ref) / (C_L_beta_e + C_L_beta_v)
-
-    slope = (1 + x_hinge/l_ref)*C_F_delta / (C_N_r - K_ac*(C_L_r - C_mtom*V))
-    print(slope)
-
-    # C1 = np.array([[0, -K_zz, 0],
-    #                [1, 0, 1/V],
-    #                [0, 0, -C_mtom]])
-    #
-    # C2 = np.array([[(C_Y_b * x_ac/l_ref - C_F_b * x_fin/l_ref), C_N_r, 0],
-    #                [0, 0, 0],
-    #                [(C_Y_b + C_F_b), 0, 0]])
-    #
-    # C3 = np.array([[C_F_delta * x_fin / l_ref],
-    #                [0],
-    #                [-C_F_delta]])
-
-    # C1 = np.array([[0, -C_mtom, 0],
-    #                [1, -1/V, 0],
-    #                [0, 0, -K_zz]])
-    #
-    # C2 = np.array([[-(C_L_beta_e + C_L_beta_v), 0, -V*C_mtom],
-    #                [0, 0, 0],
-    #                [(C_L_beta_e * x_ac/l_ref - C_L_beta_v * x_fin/l_ref), C_N_r, 0]])
-    #
-    # C3 = np.array([[C_F_delta],
-    #                [0],
-    #                [C_F_delta * (x_hinge)/l_ref]])
 
     C1 = np.array([[0, -K_zz, 0],
-                   [-C_mtom * V, 0, 0],
+                   [-mu * V, 0, 0],
                    [0, V/g, -1]])
 
-    C2 = np.array([[C_L_beta_e * x_ac/l_ref - C_L_beta_v * x_fin/l_ref, -C_N_r, 0],
-                   [-(C_L_beta_e + C_L_beta_v), C_L_r - C_mtom * V, 0],
+    C2 = np.array([[C_Y_beta_e * x_ac/l_ref - C_Y_beta_v * x_fin/l_ref, -C_N_r, 0],
+                   [-(C_Y_beta_e + C_Y_beta_v), C_Y_r - mu * V, 0],
                    [0, 0, 0]])
 
     C3 = np.array([[C_F_delta * x_hinge/l_ref],
