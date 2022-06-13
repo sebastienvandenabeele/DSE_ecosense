98e791a8ec9e385502d86930c9e5d09f7ac53193
Constantinos Aristodemou <aristodemou99@gmail.com>
2022-06-13 14:32:53 +0200

    added some function

--- Blimp/Classes/deployment.py
+++ Blimp/Classes/deployment.py
@@ -27,18 +27,31 @@ class sensors:
         return self.height*i+self.width*j+self.thickness*k
 
 
+    def spool_sizing(self, number_of_sensors):
+        r = 0.1
+        spool_width = 4*self.width
+        tape_length = (number_of_sensors*self.height + number_of_sensors*0.01)/4
+        while tape_length > 0:
+            tape_length -= pi*r**2
+            r += self.thickness
+            print(tape_length, r, "loop")
+        print(tape_length, r, spool_width)
 
-sensor_1 = sensors(height=0.065, width=0.065, thickness=0.04)
-efficient_packing_cofigs = sensor_1.packing_configuartions(648)
+    def spool_volume(self, spool_width, thickness, innerd, outerd, density):
+        disk_vol = pi*outerd**2/4*thickness
+        hollow_cylinder_vol = (pi*innerd**2/4 - pi*(innerd-2*thickness)**2/4)*spool_width
+        volume =  2*disk_vol + hollow_cylinder_vol
+        mass = volume*density
+        print(volume, mass)
 
-# x = {"1-2-3":5, "1-2-5": 6}
-# dim = list(x.keys())
-# print(dim)
-
-dimensions = list(efficient_packing_cofigs.keys())
-for i in range(10):
-    x,y,z = dimensions[i].split("-")
-    x,y,z = int(x), int(y), int(z)
-    print(f"Configuration {i+1}:")
-    print(f"Packing:{x}x{y}x{z}")
-    print(f"Dimensions:{sensor_1.height*x}x{sensor_1.width*y}x{sensor_1.thickness*z}\n")
+sensor = sensors(height=0.065, width=0.85, thickness=0.04)
+# efficient_packing_cofigs = sensor.packing_configuartions(648)
+# sensor.spool_sizing(551)
+sensor.spool_volume(0.445, 0.004, 0.03, 0.31, 1250)
+# dimensions = list(efficient_packing_cofigs.keys())
+# for i in range(10):
+#     x,y,z = dimensions[i].split("-")
+#     x,y,z = int(x), int(y), int(z)
+#     print(f"Configuration {i+1}:")
+#     print(f"Packing:{x}x{y}x{z}")
+#     print(f"Dimensions:{sensor_1.height*x}x{sensor_1.width*y}x{sensor_1.thickness*z}\n")
